c10e49f50f0e343c80b7c43ca9222be47b938a39
JBrickhouse <jbrickhouse777@gmail.com>
2020-08-26 13:46:27 -0500

    Successfully asking a question

--- index.js
+++ index.js
@@ -1,22 +1,75 @@
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const util = require("util");
+
+// const writeFileAsync = util.promisify(fs.writeFile);
+
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the Title of this project?"
+    }
 
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeReadMe(data) {
+
+
+    // TITLE
+
+
+    // DESCRIPTION
+
+
+    // TABLE OF CONTENTS
+
+
+    // INSTALLATION
 
 
 
+    // USAGE
 
 
 
-    
+    // LICENSE
+
+
+
+    // CONTRIBUTING
+
+
+
+    // TESTS
+
+
+
+    // QUESTIONS
+
+
 }
 
 // function to initialize program
-function init() {
+async function init() {
+    try {
 
+        const answers = await inquirer.prompt(questions);
+
+        console.log(answers);
+
+    //     const readMeContent = writeReadMe(answers);
+
+    //     await writeFileAsync("README.md", readMeContent);
+
+    //     console.log("Successfully wrote to README.md")
+    }
+    catch(err) {
+        console.log(err);
+    }
 }
 
 // function call to initialize program
